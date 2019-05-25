54a52baff7942cc46f96876f0986ea80437b9752
Piv <18462828+Piv200@users.noreply.github.com>
2019-05-25 11:18:36 +0930

    Add leader functionality for election.

--- src/main/java/com/vato/app/Leader.java
+++ src/main/java/com/vato/app/Leader.java
@@ -1,4 +1,13 @@
 package com.vato.app;
 
-public class Leader {
+public class Leader extends State{
+    public Leader(Node context){
+        super(context);
+    }
+
+    public void heartbeatElapsed(){
+        context.sendEmptyAppendEntries();
+        context.setTimetout(context.heartBeatTimeout, 0);
+    }
+
 }
