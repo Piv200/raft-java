7fe927420bbf4e384017f1962081ea14746feb54
Piv <18462828+Piv200@users.noreply.github.com>
2019-05-25 11:56:04 +0930

    Add private variables to node class

--- src/main/java/com/vato/app/Node.java
+++ src/main/java/com/vato/app/Node.java
@@ -1,4 +1,32 @@
 package com.vato.app;
 
+import java.util.Timer;
+import java.util.Vector;
+
+import org.json.JSONObject;
+
 public class Node {
+    private State currentState;
+    private Timer timer;
+    private MessageStrategy messageStrategy;
+
+    // Persistent States
+    private int currentTerm;
+    private String votedFor;
+    private Vector<Message> log;
+
+    // Volatile States
+    private int commitIndex;
+    private int lastApplied;
+
+    private int minTimeout;
+    private int varyTimeout;
+    private int heartbeatTimeout;
+    private JSONObject config;
+
+    private String id;
+
+    public Node(MessageStrategy messageStrategy, String configPath){
+        
+    }
 }
